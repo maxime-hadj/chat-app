4f0bc76a1a2d50269e2e2ad5be9ddeaeae41d3b8
Thomas <thomas.serdjbei@laplateforme.io>
2023-01-18 14:06:56 +0100

    Co-authored-by: Max <maxime-hadj@users.noreply.github.com>

--- App.js
+++ App.js
@@ -6,7 +6,37 @@ import RegisterScreen from './screens/RegisterScreen'
 import UsersListScreen from './screens/UsersListScreen'
 import ChatroomScreen from './screens/ChatroomScreen'
 
-const Stack = createStackNavigator()
+//Navigations components
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+import Icon from 'react-native-vector-icons/Ionicons'
+import Navigation from './src/components/Navigation';
+
+const AuthStack = createStackNavigator();
+const AppStack = createBottomTabNavigator();
+
+function AuthStackScreen() {
+  return (
+    <AuthStack.Navigator>
+      <AuthStack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+      <AuthStack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+    </AuthStack.Navigator>
+  );
+}
+
+function AppStackScreen() {
+  return (
+    <AppStack.Navigator>
+      <AppStack.Screen name="PrivateChat" component={ChatroomsListScreen} options={{ tabBarLabel: 'Chat' }} />
+      <AppStack.Screen name="Profile" component={ProfileScreen} options={{ tabBarLabel: 'Profile' }} />
+      <AppStack.Screen name="Users" component={UsersListScreen} options={{ tabBarLabel: 'Users' }} />
+    </AppStack.Navigator>
+  );
+}
+
+const Stack = createStackNavigator();
 export default function App() {
 
   return (
